fe2fd583784cfce675d268fe1d3787ac98a751b0
IvanGLS <gladiolus0707@gmail.com>
2023-02-24 12:32:43 +0200

    Created tasks for telegram bot

---
+++ book/tasks.py
@@ -0,0 +1,11 @@
+import asyncio
+
+from book.models import Borrowing
+from book.telegram_bot import notify_overdue_borrowing
+
+from celery import shared_task
+
+
+@shared_task
+def run_sync_with_api() -> None:
+    notify_overdue_borrowing(Borrowing.objects.all())
